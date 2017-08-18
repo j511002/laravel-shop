0d04407cab63fe8e91b2226970818da26e5d6e9a
JiangZH <jiangzhiheng@liweijia.com>
2017-08-18 10:44:12 +0800

    更新了cate的东西

---
+++ packages/simple-shop/brand/src/Contracts/Brand.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: coffeekizoku
+ * Date: 2017/8/18
+ * Time: 上午10:27
+ */
+
+namespace SimpleShop\Brand\Contracts;
+
+
+interface Brand
+{
+    public function getList();
+
+    
+}
